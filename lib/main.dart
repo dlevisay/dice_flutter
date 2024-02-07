ed8c742512edde6b9f30ecb4b81cf3f7d5ef71f6
dominiclevisay <dominiclevisay@gmail.com>
2024-02-06 21:10:34 -0500

    Completed Project!

---
+++ lib/main.dart
@@ -0,0 +1,28 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  return runApp(
+    MaterialApp(
+      home: Scaffold(
+        backgroundColor: Colors.red,
+        appBar: AppBar(
+          title: const Text('Dice'),
+          backgroundColor: Colors.red,
+        ),
+        body: const DicePage(),
+      ),
+    ),
+  );
+}
+
+class DicePage extends StatelessWidget {
+  const DicePage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Row(children: <Widget>[
+      Expanded(child: Image.asset('images/dice1')),
+      Expanded(child: Image.asset('images/dice2')),
+    ]);
+  }
+}
